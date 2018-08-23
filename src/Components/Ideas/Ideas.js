3a6f4cfc29e110f409baa35e90952311b8b69acf
Cale Shapera <caleshapera@gmail.com>
2018-08-23 00:40:03 -0700

    data going from form to table

--- src/Components/Ideas/Ideas.js
+++ src/Components/Ideas/Ideas.js
@@ -27,12 +27,59 @@ class Ideas extends React.Component {
  	}
 
   	handleSubmit(event) {
-    	let coolidea = document.createElement("LI");
-    	var ideacontent = document.createTextNode(this.state.title);
-    	coolidea.appendChild(ideacontent);
-    	document.getElementById("ideaslist").appendChild(coolidea);
-    	event.preventDefault();
-    	console.log(this.state);
+    
+
+  		let containerli = document.createElement('li');
+  		let list = document.getElementById("ideaslist");
+  		list.appendChild(containerli);
+  		list.insertBefore(containerli, list.childNodes[0]);
+
+  		var template = [];
+
+		template.push(
+    	'<div class="ideacard">',
+	        '<p> Title: ' + this.state.title + '</p>',
+	        '<p> Leader: '  + this.state.leader + '</p>',
+	        '<p> Description: '  + this.state.description + '</p',
+    	'</div>'
+		);
+
+		var htmlString = template.join('');
+		containerli.innerHTML = htmlString;
+
+
+
+
+
+    // 	let containerli = document.createElement("li");
+  		// let dopeidea = document.createElement("div");
+  		// let pElem = document.createElement("p");
+  		// dopeidea.classList.add("ideacard");
+  		// containerli.appendChild(dopeidea);
+  		// // let ideatitle = document.createTextNode(this.state.title);
+  		// // let idealeader = document.createTextNode(this.state.leader);
+  		// // let ideadescription = document.createTextNode(this.state.description);
+  		// pElem.innerHTML = this.state.title;
+  		// dopeidea.appendChild(pElem);
+  		// pElem.innerHTML = this.state.leader;
+  		// dopeidea.appendChild(pElem);
+  		// pElem.innerHTML = this.state.description;
+  		// dopeidea.appendChild(pElem);
+
+
+
+
+  		// // dopeidea.appendChild(idealeader);
+  		// // dopeidea.appendChild(ideadescription);
+  		// let list = document.getElementById("ideaslist");
+  		// list.appendChild(containerli);
+  		// list.insertBefore(containerli, list.childNodes[0]);
+  		
+  		this.setState({title: ''});
+  		this.setState({leader: ''});
+  		this.setState({description: ''});
+
+  		event.preventDefault();
   	}
 
 	
@@ -40,11 +87,26 @@ class Ideas extends React.Component {
 		return (
 			<div>
 				<div className="ideascard">
-					<header>Ideas</header>
+					<header className="cardheader">Ideas</header>
 					<ul id="ideaslist">
-						<li>pancake-making robot</li>
-						<li>tennis court on roof</li>
-						<li>automate david's job</li>
+						<li><div className="ideacard">
+								<p>Title: Example Title</p>
+								<p>Leader: Cale Shapera</p>
+								<p>Description: A couple of sentences about what the idea is about.</p>
+							</div>
+						</li>
+						<li><div className="ideacard">
+								<p>Title: Example Title</p>
+								<p>Leader: Cale Shapera</p>
+								<p>Description: A couple of sentences about what the idea is about.</p>
+							</div>
+						</li>
+						<li><div className="ideacard">
+								<p>Title: Example Title</p>
+								<p>Leader: Cale Shapera</p>
+								<p>Description: A couple of sentences about what the idea is about.</p>
+							</div>
+						</li>
 					</ul>
 				</div>
 
@@ -55,17 +117,18 @@ class Ideas extends React.Component {
 				</div>
 				<div className="formcontainer">
 					<form className="formnewidea" onSubmit={this.handleSubmit}>
+						<header className="cardheader">Gimme a cool idea!</header>
 						<label>
 							Title:
 							<input type="text" value={this.state.title} onChange={this.handleChangeTitle} />
 						</label>
 						<label>
 							Leader:
-							<input type="text" value={this.state.value} onChange={this.handleChangeLeader} />
+							<input type="text" value={this.state.leader} onChange={this.handleChangeLeader} />
 						</label>
 						<label>
 							Description:
-							<textarea value={this.state.value} onChange={this.handleChangeDescription} />
+							<textarea value={this.state.description} onChange={this.handleChangeDescription} />
 						</label>
 						<input type="submit" value="Submit" />
 					</form>
