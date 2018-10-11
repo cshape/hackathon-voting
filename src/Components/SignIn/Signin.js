01cf019cf874808d0c218df064bd10373f408073
gthierman <geoff.thierman@goclio.com>
2018-10-11 13:50:29 -0700

    style login card

--- src/Components/SignIn/Signin.js
+++ src/Components/SignIn/Signin.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 
 const SignIn = ({ onRouteChange }) => {
 	return (
-		<div className="card">
-			<h1>Login</h1>
-			<Link to="/ideas">
-				<button className="button">Sign In</button>
+		<div className="card sign-in-card">
+			<div class="card-title"><h1>Login</h1></div>
+			<input type="text" placeholder="Email" className="spacing-stack-m"/>
+			<input type="password" placeholder="Password" className="spacing-stack-m"/>
+			<Link to="/ideas" className="button">
+				Sign in
 			</Link>
 		</div>
 	);
