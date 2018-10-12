7dd503cec277d1b99c3dd26b4f7162aefa1557df
gthierman <geoff.thierman@goclio.com>
2018-10-12 11:37:48 -0700

    Added a card component

---
+++ src/Components/UI/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Card extends React.Component {
+	constructor(props) {
+		super(props);
+		this.title = null
+		this.renderCardTitle = this.renderCardTitle.bind(this);
+	}
+
+	renderCardTitle() {
+		if (this.props.title) {
+			return <div class="card-title"><h1>{this.props.title}</h1></div>
+		}
+	}
+	render() {
+		return (
+			<div className="card">
+				{this.renderCardTitle()}
+				{this.title}
+			</div>
+		);
+  }
+}
+
+
+export default Card;
