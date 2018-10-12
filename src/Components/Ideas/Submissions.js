70f991f0e24a30cc0c441b5e77df6370a70033bc
Drew Goddyn <drew.goddyn@clio.com>
2018-10-12 11:25:33 -0700

    delete by id

--- src/Components/Ideas/Submissions.js
+++ src/Components/Ideas/Submissions.js
@@ -15,69 +15,66 @@ class Submissions extends React.Component {
    	}
 
 
-deleteIdea(event) {
-	let button = event.target;
-	let idKey = button.id;
-	let idea = button.parentNode.firstChild.nextSibling.textContent;
-	axios.delete('http://localhost:3001/api/ideas', {
-		data: { name: idea }
-		})
-		.then(response => {
-			console.log(response, "idea deleted.")
-		})
-		.catch(err => {
-			console.log(err, "idea not deleted.")
-		})
-	console.log(idea);
+  deleteIdea(id, event) {
+    debugger
+  	let button = event.target;
+  	let idKey = button.id;
+    let url = `http://localhost:3001/api/idea/${id}`
+  	axios.delete(url)
+  		.then(response => {
+  			console.log(response, "idea deleted.")
+  		})
+  		.catch(err => {
+  			console.log(err, "idea not deleted.")
+  		})
 
-	this.setState(() => ({
-		submissions: this.state.submissions.filter(match => match.key !== idKey),
-	}))
-}
+  	this.setState(() => ({
+  		submissions: this.state.submissions.filter(match => match.key !== idKey),
+  	}))
+  }
 
-   		componentDidMount() {
+  componentDidMount() {
 
+  			fetch('http://localhost:3001/api/ideas')
+  			.then(results => {
+  				return results.json();
+  			}).then(data => {
+  				let submissions = data.map((idea, i) => {
+           let path = "/ideasShow/" + idea._id;
+           let ideaId = idea._id
+  	        return(
 
+  	          	<tr className="ideaSubmission" key={i} id={idea.id}>
+  								<td>
+                    <Link to={path}>
+                      <a>{idea.name}</a>
+                    </Link><br/>
+  									<span class="type-subdued type-small">Owned by {idea.leader}</span>
+  								</td>
+  								<td>
+  									<div className="avatar">
+  										<div className="avatar-picture">
+  											C
+  										</div>
+  										<div className="avatar-picture">
+  											C
+  										</div>
+  										<div className="avatar-picture">
+  											C
+  										</div>
+  									</div>
+  								</td>
+  								<td><button onClick={this.deleteIdea.bind(this,ideaId)} id={i} className="button button__small">Delete</button></td>
+  	          	</tr>
 
-   			fetch('http://localhost:3001/api/ideas')
-   			.then(results => {
-   				return results.json();
-   			}).then(data => {
-   				let submissions = data.map((idea, i) => {
-             let path = "/ideasShow/" + idea._id;
-			        return(
+            )
 
-			          	<tr className="ideaSubmission" key={i} id={idea.id}>
-										<td>
-                      <Link to={path}>
-                        <a>{idea.name}</a>
-                      </Link><br/>
-											<span class="type-subdued type-small">Owned by {idea.leader}</span>
-										</td>
-										<td>
-											<div className="avatar">
-												<div className="avatar-picture">
-													C
-												</div>
-												<div className="avatar-picture">
-													C
-												</div>
-												<div className="avatar-picture">
-													C
-												</div>
-											</div>
-										</td>
-										<td><button onClick={this.deleteIdea.bind(this)} id={i} className="button button__small">Delete</button></td>
-			          	</tr>
-
-		          )
-
-			    })
-			        this.setState({submissions: submissions.reverse()});
-			  // console.log(submissions);
-			  console.log(this.state.submissions);
-   			})
-   		}
+  	    })
+  	       this.setState({submissions: submissions.reverse()});
+  	       // console.log(submissions);
+  	       console.log(this.state.submissions);
+  			})
+  }
 
    		// componentDidUpdate() {
    		// 	let deleteIdea = this.props.deleteIdea.bind(this);
