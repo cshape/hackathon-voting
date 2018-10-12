e9fb0876eedca758dfe726af5a231b13dae141bd
Cale Shapera <caleshapera@gmail.com>
2018-10-11 17:36:54 -0700

    Updates

---
+++ precache-manifest.8bfba90a71094a39d129848e3ad06c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83006c133dae1dd9968f",
+    "url": "/hackathon-voting/static/js/runtime~main.83006c13.js"
+  },
+  {
+    "revision": "4811888a700b46218b1f",
+    "url": "/hackathon-voting/static/js/main.4811888a.chunk.js"
+  },
+  {
+    "revision": "ec966842d0cf7e7b359d",
+    "url": "/hackathon-voting/static/js/1.ec966842.chunk.js"
+  },
+  {
+    "revision": "4811888a700b46218b1f",
+    "url": "/hackathon-voting/static/css/main.b064b499.chunk.css"
+  },
+  {
+    "revision": "6c340fcb339bd4ea06830aea53a6ebf5",
+    "url": "/hackathon-voting/index.html"
+  }
+];
