7e63414af7f87a8a911e7bd59202c5759f4417fe
Cale Shapera <caleshapera@gmail.com>
2019-02-16 16:05:31 -0800

    Updates

---
+++ precache-manifest.95640bebfc88b162db5a8692ba27bf58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f5fff7a9fdcd204618b04cddba94f3f",
+    "url": "/hackathon-forum/static/media/hackidea.0f5fff7a.jpg"
+  },
+  {
+    "revision": "36b08184c2279f2a43b9",
+    "url": "/hackathon-forum/static/js/runtime~main.36b08184.js"
+  },
+  {
+    "revision": "82a9bc314968aa526379",
+    "url": "/hackathon-forum/static/js/main.82a9bc31.chunk.js"
+  },
+  {
+    "revision": "62d24dbd0deb92e7760f",
+    "url": "/hackathon-forum/static/js/1.62d24dbd.chunk.js"
+  },
+  {
+    "revision": "82a9bc314968aa526379",
+    "url": "/hackathon-forum/static/css/main.7f35ece2.chunk.css"
+  },
+  {
+    "revision": "62d24dbd0deb92e7760f",
+    "url": "/hackathon-forum/static/css/1.d3de4d40.chunk.css"
+  },
+  {
+    "revision": "cc23df6ef9c9e91bdf11d4aef673aa12",
+    "url": "/hackathon-forum/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/hackathon-forum/precache-manifest.bc19686ab5b3010927243c235ef94ff3.js"
+  "/hackathon-forum/precache-manifest.95640bebfc88b162db5a8692ba27bf58.js"
 );
 
 workbox.clientsClaim();
